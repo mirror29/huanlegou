1cad9313446dc77649d82ff9349e762ea5dbc96a
mirror29 <9263220181@qq.com>
2018-10-18 10:32:18 +0800

    Initial commit

---
+++ billing-system/src/components/HelloWorld.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <div class="block">
+        <span class="demonstration">不显示间断点</span>
+        <el-slider
+          v-model="value6"
+          :step="10">
+        </el-slider>
+      </div>
+      <div class="block">
+        <span class="demonstration">显示间断点</span>
+        <el-slider
+          v-model="value7"
+          :step="10"
+          show-stops>
+        </el-slider>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      value6: 0,
+      value7: 0
+    }
+  }
+}
+</script>
